94e1ade3d5be8a5bc0a38419dd2b0e2fa99e57b8
krishna <krishnamaurya31@gmail.com>
2023-06-30 11:28:20 +0530

    fixed design issues

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -84,7 +84,7 @@ const Products = () => {
 
         {filter.map((product) => {
           return (
-            <div id={product.id} key={product.id} className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+            <div id={product.id} key={product.id} className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4" style={{width:"25%"}} >
               <div className="card text-center h-100" key={product.id}>
                 <img
                   className="card-img-top p-3"
